82e30b43d11b61bdf8dc1edced45646aac8c7678
dorcas1 <dorcasoyebokun@gmail.com>
2017-11-15 13:51:57 +0000

    testing my gitignore

--- www/dbtest.php
+++ www/dbtest.php
@@ -1,5 +1,7 @@
 <form id="register" method="POST", encrypte="multipart/form-data">
 <p> Please upload a picture</p>
-<input type="file"
+<input type="file" name "pics">
+
+<input type="submit" name="save">
 
 </form>
