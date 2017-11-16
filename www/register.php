6c23423932a05e77c5cb8b4ff3e7c40cfd043ec6
dorcas1 <dorcasoyebokun@gmail.com>
2017-11-16 13:30:43 +0000

    validating register.php of the template

--- www/register.php
+++ www/register.php
@@ -1,41 +1,40 @@
 
-    $page_title = "Register";
-    
-    include ('includes/header.php');
+  
     <?php
-    if (array_key_exists('register, $_POST')) {
-        $errors = [];
+      $page_title = "Register";
+      
+      include ('includes/header.php');
+      $errors = [];
+    if (array_key_exists('register', $_POST)) {
+       
 
         if (empty($POST['fname'])) {
-            $errors[] = "please enter your firstname";
+            $errors['fname'] = "please enter your firstname";
         }
 
         if(empty($_POST['lname'])) {
-            $errors[] = "please enter your lastname";
+            $errors['lname'] = "please enter your lastname";
         }
 
         if(empty($_POST['email'])) {
-            $errors[] = "please enter your email";
+            $errors[email] = "please enter your email";
         }
 
         if(empty($_POST['password'])) {
-            $errors[] = "please enter your password";
+            $errors['password'] = "please enter your password";
         }
         if(empty($_POST['pword'])) {
-            $errors[] = "please confirm your password";
+            $errors['pword'] = "please confirm your password";
         }
         
         if(empty($errors)) {
             #do database stuff
         }else {
             foreach($errors as $err) {
-                echo $err.'</br>'
+                echo $err.'</br>';
             }
         }
     }
-
-
-
 
     
 ?>
@@ -45,26 +44,43 @@
 		<hr>
 		<form id="register"  action ="register.php" method ="POST">
 			<div>
+                <?php if (isset($errors['fname'])) {echo '<p class = "err">'.$errors['fname'].'</p>'; } 
+                ?>
 				<label>first name:</label>
 				<input type="text" name="fname" placeholder="first name">
 			</div>
 			<div>
-				<label>last name:</label>	
+                <?php if (isset($errors['lname'])) {echo '<p class = "err">'.$errors['lname'].'</p>'; } 
+                ?>
+            
+                <label>last name:</label>	
 				<input type="text" name="lname" placeholder="last name">
-			</div>
+                
+            </div>
 
 			<div>
+                <?php if (isset($errors['email'])) {echo '<p class = "err">'.$errors['email'].'</p>'; } 
+                ?>
+
 				<label>email:</label>
 				<input type="text" name="email" placeholder="email">
-			</div>
+            
+            </div>
 			<div>
 				<label>password:</label>
 				<input type="password" name="password" placeholder="password">
-			</div>
+            
+                <?php if (isset($errors['password'])) {echo '<p class = "err">'.$errors['password'].'</p>'; } 
+                ?>
+            </div>
  
 			<div>
 				<label>confirm password:</label>	
-				<input type="password" name="pword" placeholder="password">
+                <input type="password" name="pword" placeholder="password">
+                
+                
+                <?php if (isset($errors['pword'])) {echo '<p class = "err">'.$errors['pword'].'</p>'; } 
+                ?>
 			</div>
 
 			<input type="submit" name="register" value="register">
